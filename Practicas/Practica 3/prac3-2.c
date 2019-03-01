18232e894df7e2d33333a64e7fb7499a87bccec5
Ignacio Padilla <chavez.ignacio@uabc.edu.mx>
2019-02-28 16:03:55 -0800

    v 1.1

--- Practicas/Practica 3/prac3-2.c
+++ Practicas/Practica 3/prac3-2.c
@@ -4,29 +4,51 @@ extern unsigned char peek(unsigned int segment, unsigned int offset);
 typedef unsigned char byte;
 typedef unsigned int word;
 
-int main(void) { return 0; }
+int main(void) {
+        return 0;
+}
 
 /***************************
 **  Function : dataBusTest()
 **
-**
+**  Check a 8 bits data bus in the given address specified
+**  by the segment and offset.
 **
 **
 **
 **
 ** Returns : 0 if the test succeeds,
-**            otherwise the first failed pattern.
+**
 **
 **
 ***************************/
-byte dataBusTest(const word segment, const word offset) {
-  byte pattern, success = 0;
+byte dataBusTest(const word segment,const word offset) {
+        byte i,j,pattern,success;
+        const byte tests[] = {0x0,0xAA};
 
-  for (pattern = 0x01; pattern && !success; pattern <<= 1) {
-    poke(segment, offset, pattern);
+        i = j = success = 0; /***zero means success*/
 
-    success = (pattern == peek(segment, offset)) success : pattern;
-  }
+        while (i++ < 2 && !success) {
+                while(j++ < 2 && !success) {
+                        pattern = (j) ? ~tests[i] : tests[i];
+                        poke(segment,offset,pattern);
+                        /*****if the same pattern is read back correctly, success is set to 0 otherwise
+                           success is set to the binary representation of the wrong lines.
+                           Example if the line 0 and 7 are wrong then success is set to 0x81 (1000-0001).
+                         ****/
+                        success = (pattern == peek(segment, offset)) success:pattern ^ peek(segment, offset);
+                }
+        }
 
-  return success;
+        return success;
+}
+
+
+
+
+word addressBusTest(const word segment,const word offset,byte bus_lines){
+        word pattern, success = 0;
+
+
+        return success;
 }
